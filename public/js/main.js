763bdb90f6fc5bf378401b84afe7556eb8fb1979
PrefeX <christianrelling@hotmail.com>
2017-03-13 13:14:53 +0100

    bugfix

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,31 @@
 function isExternalLink(link) {
-	return link.search('http') != -1;
+    return link.search('http') != -1;
 }
 
 function loadPage(link) {
-	$('main').load('/ajax/page' + link);
-	history.pushState({}, '', link);
+    $('main').load('/ajax/page' + link);
+    history.pushState({}, '', link);
 }
 
 $(document)
 
 .on('click', 'a', function(e) {
-	var link = $(this).attr('href');
-	if (isExternalLink(link)) {
-		return;
-	}
-	e.preventDefault();
-	loadPage(link);
+    var link = $(this).attr('href');
+    if (isExternalLink(link)) {
+        return;
+    }
+    e.preventDefault();
+    loadPage(link);
+})
+
+.on('keyup', '.input-check', function() {
+    this.setAttribute('value', this.value);
 })
 
 ;
 
 $(window).bind('popstate', function(e) {
-	location.reload();
+    location.reload();
 });
 
 
