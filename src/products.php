878a0bf5583efc22916601d3c0f2e41e3dc947cf
pr0xity <pr0xity@live.no>
2017-03-15 13:22:59 +0100

    added more preview photos for theme_sakarias.

--- src/products.php
+++ src/products.php
@@ -94,8 +94,12 @@ $PRODUCTS = [
 		'details' => '<p>This is a theme that is intended to be a website about people or maybe even your pet. It can also be used as a blog or product site. You have a navigation bar on the left side, which is always visible and makes it easy to navigate the site.</p>',
 		'gallery' => [
 			[
-				'src' => '/images/pexels-photo-28245.jpg',
-				'alt' => 'Nice Photo'
+				'src' => '/images/theme_sakarias/blog.png',
+				'alt' => 'Preview of the blog page.'
+			],
+			[
+				'src' => '/images/theme_sakarias/contact.png',
+				'alt' => 'Preview of the contact page.'
 			]
 		],
 		'reviews' => [
